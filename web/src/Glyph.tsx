d804ac58ead7bc7697669ff2fb2a482088ceafb7
Marcelo Lima <marcelowind@gmail.com>
2020-08-25 21:08:32 +0200

    Allow svg in Glyph

--- web/src/Glyph.tsx
+++ web/src/Glyph.tsx
@@ -1,16 +1,19 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, SVGProps } from 'react'
 
 import './Glyph.css'
 
 interface IProps {
-  src: string
+  src: string | FunctionComponent<SVGProps<SVGSVGElement>>
 }
 
-const Glyph: FunctionComponent<IProps> = (props) =>
+const Glyph: FunctionComponent<IProps> = (props) => {
+  console.log(props.src)
+  return (
   <div className="glyph baseline">
-    <img src={props.src} alt='' />
+    {typeof props.src === 'string' ? <img src={props.src} alt='' /> : <props.src />}
     {' '}
     { props.children }
   </div>
+  )}
 
 export default Glyph
