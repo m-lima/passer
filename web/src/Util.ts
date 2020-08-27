c872f6db64adef717913ec558be962e5149ce5a4
Marcelo Lima <marcelowind@gmail.com>
2020-08-27 09:39:14 +0200

    Add util functions

---
+++ web/src/Util.ts
@@ -0,0 +1,6 @@
+export const sizeToString = (size: number) =>
+  size < 1024
+    ? `${size} B`
+    : size < 1024 * 1024
+      ? `${(size / 1024).toFixed(1)} KiB`
+      : `${(size / 1024 / 1024).toFixed(1)} MiB`
