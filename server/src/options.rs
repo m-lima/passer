b7649497a894ee432d1e12396be17b56b8f7b724
Marcelo Lima <marcelowind@gmail.com>
2020-08-30 19:37:58 +0200

    If serving front-end, must supply path

--- server/src/options.rs
+++ server/src/options.rs
@@ -18,8 +18,24 @@ pub struct Options {
     /// Selects the number of threads to use. Zero for automatic
     #[clap(short, long, default_value = "0")]
     pub threads: u8,
+
+    /// The directory of the front-end content
+    #[cfg(feature = "host-frontend")]
+    #[clap(short, long, parse(try_from_str = to_path))]
+    pub web_path: std::path::PathBuf,
 }
 
 fn to_cors(value: &str) -> Result<hyper::header::HeaderValue, hyper::header::InvalidHeaderValue> {
     hyper::header::HeaderValue::from_str(value)
+}
+
+#[cfg(feature = "host-frontend")]
+fn to_path(value: &str) -> Result<std::path::PathBuf, &'static str> {
+    let path = std::path::PathBuf::from(value);
+
+    if path.is_dir() {
+        Ok(path)
+    } else {
+        Err("path is not a directory")
+    }
 }
