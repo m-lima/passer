1c7ce2a53b4b9b3f4b1cddbeeedfc69c4f1c6e8e
Marcelo Lima <m-lima@users.noreply.github.com>
2026-01-18 17:08:33 +0100

    Update wasm crates

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -12,8 +12,17 @@
 //! `Encrypted -> Decrypt() -> Decompress() -> Deserialize() -> InnerPack -> Pack`
 //! Pack is then accessible from JS through wasm bindgen
 
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+type JString = js_sys::JsString;
+#[cfg(not(target_arch = "wasm32"))]
+type JString = String;
+
+#[cfg(target_arch = "wasm32")]
+type JVec = js_sys::Uint8Array;
+#[cfg(not(target_arch = "wasm32"))]
+type JVec = Vec<u8>;
 
 pub enum Error {
     FailedToProcess,
@@ -69,54 +78,54 @@ impl Key {
     }
 
     #[wasm_bindgen]
-    pub fn to_base64(&self) -> js_sys::JsString {
+    pub fn to_base64(&self) -> JString {
         let mut key_bytes = [0; 44];
         key_bytes[..32].copy_from_slice(&self.key);
         key_bytes[32..].copy_from_slice(&self.nonce);
-        base64::Engine::encode(&base64::engine::general_purpose::URL_SAFE_NO_PAD, key_bytes).into()
+        let out =
+            base64::Engine::encode(&base64::engine::general_purpose::URL_SAFE_NO_PAD, key_bytes);
+        JString::from(out)
     }
 
-    fn encrypt(&self, pack: &SerdePack) -> Result<Encrypted, JsValue> {
+    fn encrypt(&self, pack: SerdePack<'_>) -> Result<Encrypted, JsValue> {
         use chacha20poly1305::aead::Aead;
 
-        let binary = bincode::serde::encode_to_vec(pack, bincode::config::standard())
-            .map_err(|_| Error::FailedToProcess.into_js_value())?;
+        let binary = pack.serialize();
         let compressed = miniz_oxide::deflate::compress_to_vec(&binary, 8);
         let cipher = <chacha20poly1305::ChaCha20Poly1305 as chacha20poly1305::KeyInit>::new(
             &self.key.into(),
         );
 
-        Ok(Encrypted(
-            cipher
-                .encrypt(&self.nonce.into(), compressed.as_slice())
-                .map_err(|_| Error::FailedToProcess.into_js_value())?,
-        ))
+        cipher
+            .encrypt(&self.nonce.into(), compressed.as_slice())
+            .map(|payload| Encrypted::new(&payload))
+            .map_err(|_| Error::FailedToProcess.into_js_value())
     }
 
     #[wasm_bindgen]
     pub fn encrypt_string(&self, name: &str, data: &str) -> Result<Encrypted, JsValue> {
-        let pack = {
-            let data = Vec::from(data);
-            let size = data.len();
-            SerdePack {
-                plain_message: true,
-                name: name.into(),
-                size,
-                data,
-            }
-        };
-        self.encrypt(&pack)
+        self.encrypt(SerdePack {
+            plain_message: true,
+            name,
+            size: data
+                .len()
+                .try_into()
+                .map_err(|_| Error::FailedToProcess.into_js_value())?,
+            data: data.as_bytes(),
+        })
     }
 
     #[wasm_bindgen]
     pub fn encrypt_file(&self, name: &str, data: &[u8]) -> Result<Encrypted, JsValue> {
-        let pack = SerdePack {
+        self.encrypt(SerdePack {
             plain_message: false,
-            name: name.into(),
-            size: data.len(),
-            data: data.into(),
-        };
-        self.encrypt(&pack)
+            name,
+            size: data
+                .len()
+                .try_into()
+                .map_err(|_| Error::FailedToProcess.into_js_value())?,
+            data,
+        })
     }
 
     #[wasm_bindgen]
@@ -130,30 +139,96 @@ impl Key {
         let decompressed = miniz_oxide::inflate::decompress_to_vec(&decrypted)
             .map_err(|_| Error::FailedToProcess.into_js_value())?;
 
-        bincode::serde::decode_from_slice(&decompressed, bincode::config::standard())
-            .map(|(d, _)| d)
+        SerdePack::deserialize(&decompressed)
             .map(Pack::new)
-            .map_err(|_| Error::FailedToProcess.into_js_value())
+            .ok_or_else(|| Error::FailedToProcess.into_js_value())
     }
 }
 
 #[wasm_bindgen]
-pub struct Encrypted(Vec<u8>);
+pub struct Encrypted {
+    payload: JVec,
+}
+
+impl Encrypted {
+    fn new(payload: &[u8]) -> Self {
+        let payload = JVec::from(payload);
+        Self { payload }
+    }
+}
 
 #[wasm_bindgen]
 impl Encrypted {
-    pub fn payload(&self) -> js_sys::Uint8Array {
-        unsafe { js_sys::Uint8Array::view(&self.0) }
+    pub fn payload(&self) -> JVec {
+        self.payload.clone()
     }
 }
 
 /// The pack contains the data that should be encrypted and the metadata about it
-#[derive(Serialize, Deserialize)]
-struct SerdePack {
+#[derive(Clone, Copy)]
+struct SerdePack<'a> {
     plain_message: bool,
-    name: String,
-    size: usize,
-    data: Vec<u8>,
+    name: &'a str,
+    size: u32,
+    data: &'a [u8],
+}
+
+impl<'a> SerdePack<'a> {
+    fn serialize(&self) -> Vec<u8> {
+        let mut vec = Vec::with_capacity(
+            1 + (size_of::<usize>() + self.name.len())
+                + size_of::<usize>()
+                + (size_of::<usize>() + self.data.len()),
+        );
+        vec.push(if self.plain_message { u8::MAX } else { 0 });
+        vec.extend_from_slice(&self.name.len().to_le_bytes());
+        vec.extend_from_slice(self.name.as_bytes());
+        vec.extend_from_slice(&self.size.to_le_bytes());
+        vec.extend_from_slice(&self.data.len().to_le_bytes());
+        vec.extend_from_slice(self.data);
+        vec
+    }
+
+    fn deserialize(mut bytes: &'a [u8]) -> Option<Self> {
+        let plain_message = (!bytes.is_empty()).then(|| bytes[0] > 0)?;
+        bytes = &bytes[1..];
+
+        let name = {
+            let len = (bytes.len() >= size_of::<usize>()).then(|| {
+                let mut number = [0; size_of::<usize>()];
+                number.copy_from_slice(&bytes[..size_of::<usize>()]);
+                usize::from_le_bytes(number)
+            })?;
+            bytes = &bytes[size_of::<usize>()..];
+            let data = (bytes.len() >= len).then(|| &bytes[..len])?;
+            bytes = &bytes[len..];
+            str::from_utf8(data).ok()?
+        };
+
+        let size = (bytes.len() >= size_of::<u32>()).then(|| {
+            let mut number = [0; size_of::<u32>()];
+            number.copy_from_slice(&bytes[..size_of::<u32>()]);
+            u32::from_le_bytes(number)
+        })?;
+        bytes = &bytes[size_of::<u32>()..];
+
+        let data = {
+            let len = (bytes.len() >= size_of::<usize>()).then(|| {
+                let mut number = [0; size_of::<usize>()];
+                number.copy_from_slice(&bytes[..size_of::<usize>()]);
+                usize::from_le_bytes(number)
+            })?;
+            bytes = &bytes[size_of::<usize>()..];
+            (bytes.len() >= len).then(|| &bytes[..len])?
+        };
+
+        Some(Self {
+            plain_message,
+            name,
+            size,
+            data,
+        })
+    }
 }
 
 /// The pack contains the data that should be encrypted and the metadata about it
@@ -161,31 +236,49 @@ struct SerdePack {
 /// This struct exist to create a wasm interface to the inner pack
 #[wasm_bindgen]
 pub struct Pack {
-    inner: SerdePack,
+    plain_message: bool,
+    name: JString,
+    size: u32,
+    data: JVec,
 }
 
 impl Pack {
-    fn new(inner: SerdePack) -> Self {
-        Self { inner }
+    fn new(inner: SerdePack<'_>) -> Self {
+        let SerdePack {
+            plain_message,
+            name,
+            size,
+            data,
+        } = inner;
+
+        let name = name.into();
+        let data = data.into();
+
+        Self {
+            plain_message,
+            name,
+            size,
+            data,
+        }
     }
 }
 
 #[wasm_bindgen]
 impl Pack {
-    pub fn plain_message(&self) -> js_sys::Boolean {
-        self.inner.plain_message.into()
+    pub fn plain_message(&self) -> bool {
+        self.plain_message
     }
 
-    pub fn name(&self) -> js_sys::JsString {
-        self.inner.name.clone().into()
+    pub fn name(&self) -> JString {
+        self.name.clone()
     }
 
-    pub fn size(&self) -> usize {
-        self.inner.size
+    pub fn size(&self) -> u32 {
+        self.size
     }
 
-    pub fn data(&self) -> js_sys::Uint8Array {
-        unsafe { js_sys::Uint8Array::view(&self.inner.data) }
+    pub fn data(&mut self) -> JVec {
+        self.data.clone()
     }
 }
 
@@ -196,10 +289,12 @@ mod tests {
         let key_bytes = (1..).take(44).collect::<Vec<u8>>();
         let key = super::Key::new(&key_bytes).unwrap();
         let encrypted = key.encrypt_string("foo", "bar").unwrap();
-        let decrypted = key.decrypt(&encrypted.0).unwrap().inner;
+        let decrypted = key.decrypt(&encrypted.payload).unwrap();
+
         assert!(decrypted.plain_message);
         assert_eq!(decrypted.name, "foo");
-        assert_eq!(decrypted.data, Vec::from("bar"));
+        assert_eq!(decrypted.size, 3);
+        assert_eq!(decrypted.data, b"bar");
     }
 
     #[test]
@@ -207,9 +302,11 @@ mod tests {
         let key_bytes = (1..).take(44).collect::<Vec<u8>>();
         let key = super::Key::new(&key_bytes).unwrap();
         let encrypted = key.encrypt_file("foo", b"bar").unwrap();
-        let decrypted = key.decrypt(&encrypted.0).unwrap().inner;
+        let decrypted = key.decrypt(&encrypted.payload).unwrap();
+
         assert!(!decrypted.plain_message);
         assert_eq!(decrypted.name, "foo");
-        assert_eq!(decrypted.data, Vec::from("bar"));
+        assert_eq!(decrypted.size, 3);
+        assert_eq!(decrypted.data, b"bar");
     }
 }
